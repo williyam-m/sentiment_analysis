357b948e8d1a4446be031e762f452484f8c7292e
williyam-m <williyam64@gmail.com>
2024-12-31 01:17:27 +0530

    fix: refactor apps

--- analysis/views.py
+++ analysis/views.py
@@ -1,18 +1,1 @@
 from django.shortcuts import render
-from .arguments import get_arguments
-from .analyzer import Analyzer
-from django.contrib.auth.decorators import login_required
-
-
-@login_required(login_url='signin')
-def index(request):
-    return render(request, "index.html")
-
-
-@login_required(login_url='signin')
-def sentiment(request):
-    if request.method == 'POST':
-        text = request.POST.get("sentence")
-        analyzer = Analyzer(will_train=False, args_dict=get_arguments())
-        sentiment, percentage = analyzer.classify_sentiment(text)
-        return render(request, "index.html", {"sentiment": sentiment, "percentage": percentage, "sentence": text})
--- sentiment_analysis/urls.py
+++ sentiment_analysis/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('analysis/', include('analysis.urls')),
+    path('test/', include('text_handler.urls')),
     path('', include('dashboard.urls')),
     path('user/', include('user.urls')),
 ]
