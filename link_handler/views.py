038655b17e6cd0ec7997a51aa7dea23c16b420bc
williyam-m <williyam64@gmail.com>
2024-12-31 09:41:25 +0530

    implement web scaper using BeautifulSoup4

--- link_handler/views.py
+++ link_handler/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from analysis.arguments import get_arguments
 from analysis.analyzer import Analyzer
+from web_scraper.views import scrape_kynhood_post
 from django.contrib.auth.decorators import login_required
 
+error_msg = ["Post not exist or something went wrong", "Link must be https://kynhood.com/post/"]
 
 @login_required(login_url='signin')
 def link_index(request):
@@ -15,8 +17,15 @@ def link_sentiment(request):
         link = request.POST.get("link")
         option = request.POST.get("option")
 
-        # Assuming your analyzer can handle URLs or process content from URLs
-        analyzer = Analyzer(will_train=False, args_dict=get_arguments())
-        sentiment, percentage = analyzer.classify_sentiment(link)  # Modify this if needed to process the link content
+        if option == "kyn":
+            content = scrape_kynhood_post(link)
+            print(content)
 
-        return render(request, "link_index.html", {"sentiment": sentiment, "percentage": percentage, "link": link})
+            if content in error_msg:
+                return render(request, "link_index.html",
+                              {"error": content})
+
+            analyzer = Analyzer(will_train=False, args_dict=get_arguments())
+            sentiment, percentage = analyzer.classify_sentiment(content)
+
+            return render(request, "link_index.html", {"sentiment": sentiment, "percentage": percentage, "content": content})
--- web_scraper/views.py
+++ web_scraper/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_kynhood_post(link):
+    # Check if the domain is correct
+    if not link.startswith("https://kynhood.com/post/"):
+        return "Link must be https://kynhood.com/post/"
+
+    try:
+        response = requests.get(link)
+
+
+        if response.status_code != 200:
+            return "Post not exist or something went wrong"
+
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        description_div = soup.title.string
+        return description_div
+
+
+    except Exception as e:
+        print(e)
+        return "Post not exist or something went wrong"
